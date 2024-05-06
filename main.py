38909c6f6a4cda0640faa490b3490bea62c92c35
inf2774 <dimitrij.pivovar@smail.th-koeln.de>
2024-05-06 14:52:38 +0200

    load problem with tsplib95

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
-
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import tsplib95 as tsplib
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+def load_problem(path):
+    return tsplib.load(path)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    problem = load_problem("TSPLIB95Data/berlin52.tsp")
+    print(problem.render())
