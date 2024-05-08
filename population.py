a994cf8790f3a76ee653271b3546c43f42021df3
dimitrijpivovar <dimitrij.pivovar@brickmakers.de>
2024-05-08 12:24:12 +0200

    add population.py to create population (init pop on flowchart)

---
+++ population.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+def get_random_permutation(nodes):
+    """
+    Wir liefern eine zufällige Permutation zurück, die als Individuum einer Population zu verstehen ist.
+    Eine Permutation ist eine Anordnung von Objekten aus einer Menge nodes
+    :param
+    nodes: Die Nodes des Problemgraphen. gr17 hat 17 Nodes, gr24 hat 24 Nodes.
+    """
+    return np.random.permutation(nodes)
+
+
+def init_pop(nodes, population_size):
+    """
+    Mit dieser Population werden wir eine Evaluation machen und danach Parents auswählen
+    Ablaufdiagram siehe 3. Vorlesung Seite 23.
+    :param
+    nodes: Die Nodes des Problemgraphen. gr17 hat 17 Nodes, gr24 hat 24 Nodes.
+    population_size: Die Größe der gewünschten Population.
+    :return
+    Array der Größe population_size, Inhalt sind zufällig generierte Individuals
+    """
+    population = []
+    for _ in range(population_size):
+        individual = get_random_permutation(nodes)
+        population.append(individual)
+    return population
