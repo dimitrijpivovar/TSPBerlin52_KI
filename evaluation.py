0ebc25475d3ce340eba9d91b5ec3807beeb600b8
dimitrijpivovar <dimitrij.pivovar@brickmakers.de>
2024-05-08 14:14:06 +0200

    add evaluation.py and get_fitness function

---
+++ evaluation.py
@@ -0,0 +1,21 @@
+import transform_matrix
+
+
+def get_fitness(individual, problem):
+    """
+    Als Teil der Evaluation benötigen wir die Fitness.
+    Fitness ist die Summe der Gewichte der Pfade in einem Individuum.
+    Optimale Fitness ist das gegebene Bounds (gr17 2085, gr24 1272)
+    Für gr24 ist die optimale Route
+    individual = np.array([15, 10, 2, 6, 5, 23, 7, 20, 4, 9, 16, 21, 17, 18, 14, 1, 19, 13, 12, 8, 22, 3, 11, 0])
+    :return:
+    """
+    distance_matrix = transform_matrix.transform_low_diag_matrix_to_block_matrix(problem)
+    fitness = 0
+    for i in range(len(individual)):
+        fitness += distance_matrix[individual[i - 1]][individual[i]]
+    """
+    Ich negiere Fitness hier, damit eine größere Fitness besser bewertet werden kann.
+    Fitness ist die Summe der Weights aller Pfade im Individum. Wir wollen eine niedrige Summe der Weights.
+    """
+    return -fitness
