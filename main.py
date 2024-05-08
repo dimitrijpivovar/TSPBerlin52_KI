e47e085f243988b9ea056066bfc1591d4bcd65ff
dimitrijpivovar <dimitrij.pivovar@brickmakers.de>
2024-05-08 13:05:07 +0200

    refactor visualization.py to conditionally generate assets

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tsplib95 as tsplib
 import population
+import visualization
 
 
 def load_problem(path):
@@ -10,7 +11,8 @@ if __name__ == '__main__':
     problem_gr24 = load_problem("TSPLIB95Data/gr24.tsp") #bounds 1272
     problem_gr17 = load_problem("TSPLIB95Data/gr17.tsp") #bounds 2085
     problem_gr24_opt = load_problem("TSPLIB95Data/gr24.opt.tour")
-    #visualization.generate_and_save(problem_gr24)
+    visualization.generate_and_save(problem_gr24)
+    visualization.generate_and_save(problem_gr17)
     population = population.init_pop(nodes=list(problem_gr24.get_nodes()), population_size=100)
 
 
--- visualization.py
+++ visualization.py
@@ -2,6 +2,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import pandas as pd
 import transform_matrix
+from os import path
 
 
 def generate_and_save(problem):
@@ -10,41 +11,43 @@ def generate_and_save(problem):
 
 
 def draw_graph(problem):
-    G = nx.Graph()
-    edge_labels = {}
-    for edge in list(problem.get_edges()):
-        if edge[0] != edge[1]:
-            weight = problem.get_weight(*edge)
-            G.add_edge(edge[0], edge[1], weight=weight)
-            edge_labels[(edge[0], edge[1])] = weight
+    path_of_file = './' + problem.name + '-graph.png'
+    if not path.exists(path_of_file):
+        G = nx.Graph()
+        edge_labels = {}
+        for edge in list(problem.get_edges()):
+            if edge[0] != edge[1]:
+                weight = problem.get_weight(*edge)
+                G.add_edge(edge[0], edge[1], weight=weight)
+                edge_labels[(edge[0], edge[1])] = weight
 
-    options = {
-        "font_size": 1,
-        "node_size": 10,
-        "node_color": "red",
-        "edgecolors": "black",
-        "linewidths": 0.1,
-        "width": 0.2,
-    }
-    nx.draw_networkx(G, **options)
+        options = {
+            "font_size": 1,
+            "node_size": 10,
+            "node_color": "red",
+            "edgecolors": "black",
+            "linewidths": 0.1,
+            "width": 0.2,
+        }
+        nx.draw_networkx(G, **options)
 
-    nx.draw_networkx_edge_labels(G, pos=nx.spring_layout(G), edge_labels=edge_labels, font_size=1)
+        nx.draw_networkx_edge_labels(G, pos=nx.spring_layout(G), edge_labels=edge_labels, font_size=1)
 
-    ax = plt.gca()
-    ax.margins(0.05)
-    plt.axis("on")
-    plt.savefig(problem.name, dpi=600)
-    plt.show()
+        ax = plt.gca()
+        ax.margins(0.05)
+        plt.axis("on")
+        plt.savefig(problem.name + '-graph', dpi=600)
 
 
 def create_table(problem):
-    distance_matrix = transform_matrix.transform_low_diag_matrix_to_block_matrix(problem)
-    dataframe = pd.DataFrame(distance_matrix[0:])
-    fig, ax = plt.subplots()
-    ax.axis('off')
-    table = ax.table(cellText=dataframe.values, loc='center')
-    table.auto_set_font_size(False)
-    table.set_fontsize(4)
-    table.scale(1, 1)
-
-    plt.savefig('table.png', dpi=600)
+    path_of_file = './' + problem.name + '-table.png'
+    if not path.exists(path_of_file):
+        distance_matrix = transform_matrix.transform_low_diag_matrix_to_block_matrix(problem)
+        dataframe = pd.DataFrame(distance_matrix[0:])
+        fig, ax = plt.subplots()
+        ax.axis('off')
+        table = ax.table(cellText=dataframe.values, loc='center')
+        table.auto_set_font_size(False)
+        table.set_fontsize(4)
+        table.scale(1, 1)
+        plt.savefig(problem.name + '-table', dpi=600)
