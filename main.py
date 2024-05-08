8a5b5743495ac3e1c162f3ac729e00ce9b3a1b24
dimitrijpivovar <dimitrij.pivovar@brickmakers.de>
2024-05-08 12:24:48 +0200

    clean up main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import tsplib95 as tsplib
-import draw_graph
-import get_weights
+import population
 
 
 def load_problem(path):
@@ -11,14 +10,9 @@ if __name__ == '__main__':
     problem_gr24 = load_problem("TSPLIB95Data/gr24.tsp") #bounds 1272
     problem_gr17 = load_problem("TSPLIB95Data/gr17.tsp") #bounds 2085
     problem_gr24_opt = load_problem("TSPLIB95Data/gr24.opt.tour")
+    #visualization.generate_and_save(problem_gr24)
+    population = population.init_pop(nodes=list(problem_gr24.get_nodes()), population_size=100)
 
-    graph_of_gr24 = problem_gr24.get_graph()
-    graph_of_gr17 = problem_gr17.get_graph()
-    #draw_graph.draw(problem_gr24)
-    #draw_graph.draw(problem_gr17)
 
-    #canonical tour = tour von 1->2->3....->1
-    print(problem_gr24.trace_canonical_tour())
-    #eigene implementation von canonical tour
-    weight_of_a_path = get_weights.get_weight_of_canonical_path(problem_gr24)
-    print(weight_of_a_path)
+
+
