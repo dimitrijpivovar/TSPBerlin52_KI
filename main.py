2eea7f5d84c03604c053b96aa19e82dac3f346b0
dimitrijpivovar <dimitrij.pivovar@brickmakers.de>
2024-05-11 13:59:21 +0200

    import logger.py and adjust print statements

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from evaluation import Evaluation
 from selection import Selection
 from visualization import Visualization
 from population import init_pop
+import logger
 import mutation
 import crossover
 
@@ -27,12 +28,9 @@ if __name__ == '__main__':
     population_size = 100
     population = init_pop(nodes=list(problem_gr24.get_nodes()), population_size=population_size)
 
-    fittest_individual_of_generation, _ = evaluation.get_fittest_individual_for_population(population)
-    print('Best individual: ', fittest_individual_of_generation)
-    print('Weight: ', -evaluation.get_fitness_for_individual(fittest_individual_of_generation))
-
+    logger.print_fittest_individual(evaluation, population)
     for i in range(generation_size):
-        print('Generation: ', i, ' has a fitness of: ', evaluation.get_fitness_for_population(population))
+        logger.print_generation_fitness(generation=i, fitness=evaluation.get_fitness_for_population(population))
         new_population = []
         for individual in selection.survival_selection(population=population, percentage=0.1):
             new_population.append(individual)
@@ -43,8 +41,5 @@ if __name__ == '__main__':
             mutation.mutate(individual=child, mutation_probability=0.33)
             new_population.append(child)
         population = new_population
-
-    fittest_individual_of_generation, _ = evaluation.get_fittest_individual_for_population(population)
-    print('Best individual: ', fittest_individual_of_generation)
-    print('Weight: ', -evaluation.get_fitness_for_individual(fittest_individual_of_generation))
+    logger.print_fittest_individual(evaluation, population)
 
